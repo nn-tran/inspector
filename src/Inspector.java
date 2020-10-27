b90c2557acfcf439835acae7116a08809ea0d9da
nguyen.tran1 <nguyen.tran1@cs.ucalgary.ca>
2020-10-27 15:44:12 -0600

    added some outputs and field handling added recursion

--- src/Inspector.java
+++ src/Inspector.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 /**
  * CPSC 501
@@ -17,23 +18,42 @@ public class Inspector {
 	}
 
 	private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+		String tab = indent(depth);
+		String cName = c.getName();
+		System.out.println(tab + "<Inspecting " + cName + ">");
+				
+		Class cSuper = c.getSuperclass();
+		if (cSuper!=null) {
+			String superName = cSuper.getName();
+			System.out.println(tab + " Superclass: " + superName);
+		}
+		Class[] interfaces = c.getInterfaces();
+		System.out.print(tab + " Interface(s):");
+		if (interfaces.length == 0) {
+			System.out.println(" None");
+		} else {
+			System.out.println();
+			for (Class i : interfaces) {
+				System.out.println(tab + "  " + i.getName());
+			}
+		}
+		constructorHandler(c, obj);
+		methodHandler(c, obj);
+		fieldHandler(c, obj, recursive, depth);
+		
 		if (c.isArray()) {
 			Class cComp = c.getComponentType();
 			int len = Array.getLength(obj);
 			for (int i = 0; i < len; ++i) {
 				Object value = Array.get(obj, i);
+			System.out.print("");
 			}
 		}
-		String name = c.getName();
-		Class superC = c.getSuperclass(); 
-		String superName = superC.getName();
-		Class[] interfaces = c.getInterfaces();
 		
-		constructorHandler(c, obj);
-		methodHandler(c, obj);
-		fieldHandler(c, obj, recursive, depth);
-		
-		
+		if (cSuper!=null) {
+			System.out.println(tab + " Inspecting superclass");
+			inspectClass(cSuper, obj, recursive, depth+1);
+		}
 		
 	}
 
@@ -59,26 +79,47 @@ public class Inspector {
 	}
 
 	private void fieldHandler(Class c, Object obj, boolean recursive, int depth) {
+		String tab = indent(depth);
 		Field[] fields = c.getDeclaredFields();
-		for (Field k : fields) {
-			String fName = k.getName();
-			Class fType = k.getType();
-			int fMod = k.getModifiers();
-			try {
-				Object value = k.get(obj);
-				if (recursive) {
+		System.out.print(tab + " Field(s):");
+		if (fields.length == 0) {
+			System.out.println(" None");
+		} else {
+			System.out.println();
+			for (Field k : fields) {
+				String fName = k.getName();
+				Class fType = k.getType();
+				int fMod = k.getModifiers();
+				System.out.println(tab + "  Name: "+ fName);
+				System.out.println(tab + "   Type: "+ fType);
+				System.out.println(tab + "   Modifiers: "+ Modifier.toString(fMod));
+				try {
+					Object value = k.get(obj);
 					Class cField = value.getClass();
-					inspectClass(cField, value, recursive, depth+1);
+					if (recursive) {
+						System.out.println(tab + "   Value: Field is class");
+						System.out.println(tab + "   Inspecting field");
+						inspectClass(cField, value, recursive, depth+1);
+					} else {
+						System.out.println(value);
+					}
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					System.out.println(tab + "   Value: not visible");
+					//e.printStackTrace();
 				}
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				System.out.println("private field");
-				e.printStackTrace();
 			}
 		}
 	}
 
+	private String indent(int depth) {
+		String res = "";
+		for (int i = 0; i < depth; ++i) {
+			res += "\t";
+		}
+		return res;
+	}
 }
