902365bab8f4990ac0429f740df890d7ef0d0c55
nguyen.tran1 <nguyen.tran1@cs.ucalgary.ca>
2020-10-27 14:23:41 -0600

    split up inspection to simplify simple array handling

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -10,40 +11,60 @@ import java.lang.reflect.Method;
  */
 public class Inspector {
 
-    public void inspect(Object obj, boolean recursive) {
-        Class c = obj.getClass();
-        inspectClass(c, obj, recursive, 0);
-    }
+	public void inspect(Object obj, boolean recursive) {
+		Class c = obj.getClass();
+		inspectClass(c, obj, recursive, 0);
+	}
 
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-    	
-    	String name = c.getName();
-    	Class superC = c.getSuperclass(); 
-    	String superName = superC.getName();
-    	Class[] interfaces = c.getInterfaces();
-    	Constructor[] constructors = c.getDeclaredConstructors();
-    	for (Constructor i : constructors) {
-    		String cName = i.getName();
-    		Class[] cExceptions = i.getExceptionTypes();
-    		Class[] cParams = i.getParameterTypes();
-    		int cMod = i.getModifiers();
-    	}
-    	
-    	Method[] methods = c.getDeclaredMethods();
-    	for (Method j : methods) {
-    		String mName = j.getName();
-    		Class[] mExceptions = j.getExceptionTypes();
-    		Class[] mParams = j.getParameterTypes();
-    		Class mRet = j.getReturnType();
-    		int cMod = j.getModifiers();
-    	}
-    	
-    	Field[] fields = c.getDeclaredFields();
-    	for (Field k : fields) {
-    		String fName = k.getName();
-    		Class fType = k.getType();
-    		int fMod = k.getModifiers();
-    		try {
+	private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+		if (c.isArray()) {
+			Class cComp = c.getComponentType();
+			int len = Array.getLength(obj);
+			for (int i = 0; i < len; ++i) {
+				Object value = Array.get(obj, i);
+			}
+		}
+		String name = c.getName();
+		Class superC = c.getSuperclass(); 
+		String superName = superC.getName();
+		Class[] interfaces = c.getInterfaces();
+		
+		constructorHandler(c, obj);
+		methodHandler(c, obj);
+		fieldHandler(c, obj, recursive, depth);
+		
+		
+		
+	}
+
+	private void constructorHandler(Class c, Object obj) {
+		Constructor[] constructors = c.getDeclaredConstructors();
+		for (Constructor i : constructors) {
+			String cName = i.getName();
+			Class[] cExceptions = i.getExceptionTypes();
+			Class[] cParams = i.getParameterTypes();
+			int cMod = i.getModifiers();
+		}
+	}
+	
+	private void methodHandler(Class c, Object obj) {
+		Method[] methods = c.getDeclaredMethods();
+		for (Method j : methods) {
+			String mName = j.getName();
+			Class[] mExceptions = j.getExceptionTypes();
+			Class[] mParams = j.getParameterTypes();
+			Class mRet = j.getReturnType();
+			int cMod = j.getModifiers();
+		}
+	}
+
+	private void fieldHandler(Class c, Object obj, boolean recursive, int depth) {
+		Field[] fields = c.getDeclaredFields();
+		for (Field k : fields) {
+			String fName = k.getName();
+			Class fType = k.getType();
+			int fMod = k.getModifiers();
+			try {
 				Object value = k.get(obj);
 				if (recursive) {
 					Class cField = value.getClass();
@@ -54,9 +75,10 @@ public class Inspector {
 				e.printStackTrace();
 			} catch (IllegalAccessException e) {
 				// TODO Auto-generated catch block
+				System.out.println("private field");
 				e.printStackTrace();
 			}
-    	}
-    }
+		}
+	}
 
 }
