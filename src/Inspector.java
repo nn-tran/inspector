3620df2fdde5f7f8bd959973807c618be085d60a
nguyen.tran1 <nguyen.tran1@cs.ucalgary.ca>
2020-10-28 14:23:06 -0600

    added some documentation fixed recursion

--- src/Inspector.java
+++ src/Inspector.java
@@ -12,21 +12,63 @@ import java.lang.reflect.Modifier;
  */
 public class Inspector {
 
+	/**
+	 * inspect object
+	 * @param obj		object inspected
+	 * @param recursive	recursion status
+	 */
 	public void inspect(Object obj, boolean recursive) {
 		Class c = obj.getClass();
 		inspectClass(c, obj, recursive, 0);
 	}
 
+	/**
+	 * inspect class of an object
+	 * @param c			class of object
+	 * @param obj		object inspected
+	 * @param recursive	recursion status
+	 * @param depth		current depth, used for recursion
+	 */
 	private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
 		String tab = indent(depth);
 		System.out.println(tab + "<Inspecting " + c.getName() + ">");
-				
+		arrayHandler(c, obj, tab);
+		superClassHandler(c, obj, recursive, depth, tab);
+		interfaceHandler(c, obj, recursive, depth, tab);
+		constructorHandler(c, obj, tab);
+		methodHandler(c, obj, tab);
+		fieldHandler(c, obj, recursive, depth, tab);
+		
+	}
+
+	/**
+	 * finds and prints superclass details
+	 * will recursively inspect superclasses
+	 * @param c			class of object
+	 * @param obj		object inspected
+	 * @param recursive	recursion status
+	 * @param depth		current depth, used for recursion
+	 * @param tab		the indentation for printing
+	 */
+	private void superClassHandler(Class c, Object obj, boolean recursive, int depth, String tab) {
 		Class cSuper = c.getSuperclass();
 		if (cSuper!=null) {
 			System.out.println(tab + " Superclass: " + cSuper.getName());
 			//System.out.println(tab + " Inspecting superclass");
 			inspectClass(cSuper, obj, recursive, depth+1);
 		}
+	}
+	
+	/**
+	 * finds and prints interface details
+	 * will recursively inspect interfaces
+	 * @param c			class of object
+	 * @param obj		object inspected
+	 * @param recursive	recursion status
+	 * @param depth		current depth, used for recursion
+	 * @param tab		the indentation for printing
+	 */
+	private void interfaceHandler(Class c, Object obj, boolean recursive, int depth, String tab) {
 		Class[] interfaces = c.getInterfaces();
 		System.out.print(tab + " Interface(s):");
 		if (interfaces.length == 0) {
@@ -39,34 +81,35 @@ public class Inspector {
 				inspectClass(i, obj, recursive, depth+1);
 			}
 		}
-		constructorHandler(c, obj, tab);
-		methodHandler(c, obj, tab);
-		fieldHandler(c, obj, recursive, depth, tab);
-		arrayHandler(c, obj, recursive, depth, tab);
-		
-		
-		
 	}
-
-	private void arrayHandler(Class c, Object obj, boolean recursive, int depth, String tab) {
+	
+	/**
+	 * finds and prints array details
+	 * @param c			class of object
+	 * @param obj		object inspected
+	 * @param tab		the indentation for printing
+	 */
+	private void arrayHandler(Class c, Object obj, String tab) {
 		if (c.isArray()) {
 			int len = Array.getLength(obj);
 			System.out.println(tab + " Object is array");
-			System.out.println(tab + "  Type: " + c.getComponentType());
-			System.out.println(tab + "  Length: " + len);
-			System.out.println(tab + "  Values:");
+			System.out.println(tab + " Type: " + c.getComponentType());
+			System.out.println(tab + " Length: " + len);
+			System.out.println(tab + " Values:");
 			for (int i = 0; i < len; ++i) {
 				Object value = Array.get(obj, i);
-				if (!recursive || value == null) {
-					System.out.println(tab + "   " + value);
-				} else {
-					System.out.println(tab + "   Inspecting array element");
-					inspectClass(value.getClass(), value, recursive, depth+1);
-				}
+				System.out.println(tab + "  " + value);
+				
 			}
 		}
 	}
 	
+	/**
+	 * finds and prints constructor details
+	 * @param c		class of object
+	 * @param obj	object inspected
+	 * @param tab	the indentation for printing
+	 */
 	private void constructorHandler(Class c, Object obj, String tab) {
 		Constructor[] constructors = c.getDeclaredConstructors();
 		System.out.print(tab + " Constructor(s):");
@@ -79,7 +122,7 @@ public class Inspector {
 				Class[] cParams = i.getParameterTypes();
 				int cMod = i.getModifiers();
 				System.out.println(tab + "  Name: " + i.getName());
-				System.out.print(tab + "  Exception(s):");
+				System.out.print(tab + "   Exception(s):");
 				if (cExceptions.length == 0) {
 					System.out.println(" None");
 				} else {
@@ -88,7 +131,7 @@ public class Inspector {
 						System.out.println(tab + "   " + j.getName());
 					}
 				}
-				System.out.print(tab + "  Parameter(s):");
+				System.out.print(tab + "   Parameter(s):");
 				if (cParams.length == 0) {
 					System.out.println(" None");
 				} else {
@@ -97,43 +140,84 @@ public class Inspector {
 						System.out.println(tab + "   " + j.getName());
 					}
 				}
+				System.out.println(tab + "   Modifier(s): "+ Modifier.toString(cMod));
+
 			}
 		}
 		
 	}
 	
+	/**
+	 * finds and prints method details
+	 * @param c		class of object
+	 * @param obj	object inspected
+	 * @param tab	the indentation for printing
+	 */
 	private void methodHandler(Class c, Object obj, String tab) {
 		Method[] methods = c.getDeclaredMethods();
-		for (Method j : methods) {
-			String mName = j.getName();
-			Class[] mExceptions = j.getExceptionTypes();
-			Class[] mParams = j.getParameterTypes();
-			Class mRet = j.getReturnType();
-			int cMod = j.getModifiers();
+		System.out.print(tab + " Method(s):");
+		if (methods.length == 0) {
+			System.out.println(" None");
+		} else {
+			System.out.println();
+			for (Method i : methods) {
+				Class[] mExceptions = i.getExceptionTypes();
+				Class[] mParams = i.getParameterTypes();
+				Class mRet = i.getReturnType();
+				int mMod = i.getModifiers();
+				System.out.println(tab + "  Name: " + i.getName());
+				System.out.print(tab + "   Exception(s):");
+				if (mExceptions.length == 0) {
+					System.out.println(" None");
+				} else {
+					System.out.println();
+					for (Class j : mExceptions) {
+						System.out.println(tab + "    " + j.getName());
+					}
+				}
+				System.out.print(tab + "   Parameter(s):");
+				if (mParams.length == 0) {
+					System.out.println(" None");
+				} else {
+					System.out.println();
+					for (Class j : mParams) {
+						System.out.println(tab + "    " + j.getName());
+					}
+				}
+				System.out.println(tab + "   Return type: " + mRet.getName());
+				System.out.println(tab + "   Modifier(s): "+ Modifier.toString(mMod));
+
+			}
 		}
 	}
-
+	
+	/**
+	 * finds and prints field details
+	 * will recursively inspect fields if recursive == true
+	 * @param c			class of object
+	 * @param obj		object inspected
+	 * @param recursive	recursion status
+	 * @param depth		current depth, used for recursion
+	 * @param tab		the indentation for printing
+	 */
 	private void fieldHandler(Class c, Object obj, boolean recursive, int depth, String tab) {
 		Field[] fields = c.getDeclaredFields();
+		Field.setAccessible(fields, true);
 		System.out.print(tab + " Field(s):");
 		if (fields.length == 0) {
 			System.out.println(" None");
 		} else {
 			System.out.println();
 			for (Field k : fields) {
-				String fName = k.getName();
-				Class fType = k.getType();
-				int fMod = k.getModifiers();
-				System.out.println(tab + "  Name: "+ fName);
-				System.out.println(tab + "   Type: "+ fType);
-				System.out.println(tab + "   Modifiers: "+ Modifier.toString(fMod));
+				System.out.println(tab + "  Name: "+ k.getName());
+				System.out.println(tab + "   Type: "+ k.getType());
+				System.out.println(tab + "   Modifier(s): "+ Modifier.toString(k.getModifiers()));
 				try {
 					Object value = k.get(obj);
-					Class cField = value.getClass();
-					if (recursive) {
+					if (recursive && value != null && !k.getType().isPrimitive()) {
 						System.out.println(tab + "   Value: Field is class");
 						System.out.println(tab + "   Inspecting field");
-						inspectClass(cField, value, recursive, depth+1);
+						inspectClass(value.getClass(), value, recursive, depth+1);
 					} else {
 						System.out.println(tab + "   Value:" + value);
 					}
@@ -149,7 +233,12 @@ public class Inspector {
 		}
 	}
 
-	private String indent(int depth) {
+	/**
+	 * make indentation based on depth
+	 * @param depth current recursion depth
+	 * @return the indent string
+	 */
+	String indent(int depth) {
 		String res = "";
 		for (int i = 0; i < depth; ++i) {
 			res += "\t";
